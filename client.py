2e975ba41e5543cfd004d630dae10c5df8f454b1
ValiantPhoenix13 <axelkh2001@hotmail.com>
2024-11-20 13:28:14 -0800

    Should be a completed project.

--- client.py
+++ client.py
@@ -1,1 +1,87 @@
 # This will be the client side of the program
+# The program will continuously ask for inputs for the server 
+
+# Import required libraries
+import sys
+import socket
+import threading
+
+# The main function that will operate as a client
+def main():
+    # Check to see if the inputs are valid
+    if (len(sys.argv) != 3):
+        print("Usage: python client.py <server_ip> <server_port>")
+        sys.exit(1)
+    host = sys.argv[1]
+    # Define the port
+    port = int(sys.argv[2])
+    # Create a network connection using TCP
+    svr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server via localhost/local computer
+    svr_socket.connect((host, port))
+    # List the possible actions in the chatroom
+    print("You've connected to the server that is a chatroom.")
+    print("Possible actions are as follows:")
+    print("JOIN <username>: Allows you to join the chatroom if there's space with the provided username.")
+    print("LIST: Shows all current active users and their usernames in the chatroom.")
+    print("MESG <username> <message>: Allows you to privately message a specific user uses their username and sending them the provided message.")
+    print("BCST <message>: Will broadcast the provided message to all users in the chatroom")
+    print("QUIT: Will remove you from the chatroom, if you're an active user, and the server will disconnect you from the user")
+    
+
+    # Defining a function to accept broadcast
+    def receive():
+        while True:
+            try:
+                message = svr_socket.recv(1024).decode('ascii')
+                print(message)
+                # Check to see if the QUIT command was sent
+                if (str(message) == "Disconnecting"):
+                    svr_socket.close()
+                    print("Disconnected")
+                    break
+                else:
+                    continue
+            except Exception as e:
+                print(e)
+                svr_socket.close()
+                break
+
+    # Define the actions the user can do
+    def write():
+        while True:
+            # Define the request to be sent to the server
+            command = input("")
+            # Send to the server
+            svr_socket.send(command.encode('ascii'))
+            # Receive the server response
+            # response = svr_socket.recv(1024)
+            # print(str(response.decode('ascii')))
+
+            # Check use case errors in commands/request
+            if sys.argv[0] == "JOIN":
+                if sys.argv != 1:
+                    print("To use JOIN must provide a username with no special characters or spaces. \nUsage: JOIN <username>")
+            if sys.argv[0] == "LIST":
+                if sys.argv != 0:
+                    print("To use LIST no other words or spaces should be provided. \nUsage: LIST")
+            if sys.argv[0] == "BCST":
+                if sys.argv < 1:
+                    print("To use BCST you must provide a message of somekind. \nUsage: BCST <message>")
+            if sys.argv[0] == "MESG":
+                if sys.argv < 2:
+                    print("To use MESG a username and message must be provided, be sure that the start of the message is seperated from the username with a space.\n Usage: MESG <username> <message>")
+            
+
+    
+    # Start both threads, one for receiving and one for commands
+    thread_receive = threading.Thread(target=receive)
+    thread_command = threading.Thread(target=write)
+    thread_command.start()
+    thread_receive.start()
+
+
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -21,15 +21,118 @@ import sys
 active_count = 0
 active_clients = []
 usernames = []
-clients = []
 
 # Method used for the server to receive request
-def handle(client_socket):
+def handle(client_socket, client_address):
+    global active_count
     while True:
-        data = client_socket.recv().decode()
-        if data.startswith("JOIN "):
-            username = data[5:]
-            JOIN(username)
+        data = client_socket.recv(1024).decode('ascii')
+
+        # Defining the JOIN command
+        if (data.startswith("JOIN ") and len(active_clients) < 10):
+            # Check to see if the username is taken
+            if data[5:] in usernames:
+                client_socket.send((f"Unfortunately {data[5:]} is already taken, please choose a different username").encode('ascii'))
+                print(f"{client_address} tried to use the username {data[5:]}, but it was already taken.")
+            elif client_socket in active_clients:
+                client_socket.send((f"You are already an active chatter").encode('ascii'))
+                print(f"{client_address} tried to JOIN, but he was already active.")
+            else:
+                username = data[5:]
+                active_count += 1
+                usernames.append(username)
+                active_clients.append(client_socket)
+                # Send a status to the client
+                client_socket.send((f"You've joined the chatroom with the username: {username}").encode('ascii'))
+                # Display the status on the server side
+                print(f"{username} has joined the chatroom with address {client_address}")
+
+        # Defining the LIST command
+        if (data == "LIST" and (client_socket in active_clients) and data[5:] != " "):
+            # Send the client who requested the active users a list of active users
+            list = "The active users include: " # Can only send one response per command str to be sent
+            for clients in range(len(active_clients)):
+                list = list + usernames[clients] + ", "
+            client_socket.send(f"{list}".encode('ascii'))
+            # Dispaly the status on the server side
+            index = active_clients.index(client_socket)
+            print(f"{usernames[index]} requested a list of active users.")
+
+        # Defining the BCST command
+        if (data.startswith("BCST ") and (client_socket in active_clients)):
+            index = active_clients.index(client_socket)
+            username = str(usernames[index])
+            broadcast = username + ": " + str(data[5:])
+            # Send a message to each active client
+            for clients in active_clients:
+                clients.send((f"{broadcast}").encode('ascii'))
+
+            # Display the status on the server
+            index = active_clients.index(client_socket)
+            print(f"{usernames[index]} broadcasted a message.")
+
+        # Defining the MESG command
+        if (data.startswith("MESG ") and (client_socket in active_clients)):
+            contents = data[5:]
+            # Seperate the username from the message
+            contents_list = contents.split()
+            if contents_list[0] in usernames:
+                # Find the address of the recipient
+                index_recipient = usernames.index(contents_list[0])
+                recipient = active_clients[index_recipient]
+                # Find the index of the sender
+                index_sender = active_clients.index(client_socket)
+                sender = usernames[index_sender]
+                # Build the message
+                message = (" ".join(str(words) for words in contents_list[1:]))
+                to_send = sender + ": " + message
+                # Send the message
+                recipient.send((to_send).encode('ascii'))
+                client_socket.send((f"Your message was sent.").encode('ascii'))
+                # Display the status on the server side
+                print(f"{sender} sent a private message to {usernames[index_recipient]}.")
+            else:
+                # The username is not an active user
+                client_socket.send((f"{contents_list[0]} is not an active user."))
+                # Display the status on the server side
+                index = active_clients.index(client_socket)
+                print(f"{usernames[index]} tried to send a private message to an unactive user.")
+
+        # Catch any users trying to use chatroom commands when not an active user
+        if (data.startswith("LIST") and (client_socket not in active_clients)):
+            # Tell client how to join chatroom
+            client_socket.send((f"To use LIST you must be an active user. Use JOIN <username> to become active and if there's space you will be added.").encode('ascii'))
+            # Display status on server
+            print(f"{client_socket} attempted to use a command that required them to be an active chatter.")
+        if (data.startswith("BCST") and (client_socket not in active_clients)):
+            # Tell client how to join chatroom
+            client_socket.send((f"To use BCST you must be an active user. Use JOIN <username> to become active and if there's space you will be added.").encode('ascii'))
+            # Display status on server
+            print(f"{client_socket} attempted to use a command that required them to be an active chatter.")
+        if (data.startswith("MESG") and (client_socket not in active_clients)):
+            # Tell client how to join chatroom
+            client_socket.send((f"To use MESG you must be an active user. Use JOIN <username> to become active and if there's space you will be added.").encode('ascii'))
+            # Display status on server
+            print(f"{client_socket} attempted to use a command that required them to be an active chatter.")
+
+        # Defining the QUIT command
+        if (data.startswith("QUIT")):
+            # Decrement active users
+            active_count -= 1
+            # Remove active user's data from chatroom
+            index = active_clients.index(client_socket)
+            active_clients.remove(index)
+            usernames.remove(index)
+
+            # Send status to client
+            client_socket.send(("Disconnecting").encode('ascii'))
+            # Display status on server
+            print(f"{client_socket} has disconnected from the chatroom. There are now {active_count} active users.")
+            break
+
+        else:
+            client_socket.send(("Unknown command").encode('ascii'))
+    client_socket.close()
             
 
 # Creating the server
@@ -37,19 +140,22 @@ def create_server(port):
     # Creating a socket on the network with a TCP connection
     svr_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Binding the server to an address and port
-    svr_socket.bind("127.0.0.1", port)
+    svr_socket.bind(("127.0.0.1", port))
     # Server will start listening
-    svr_socket.listen(1)
+    svr_socket.listen(5)
     
     print("The server is listening on port: ", port)
     
     # Continuously search for clients
     while True:
         client_socket, client_address = svr_socket.accept()
+        slots_available = 10 - len(active_clients)
         print(f"Accepted connection from: {client_address}")
+        print(f"There are {slots_available} spots available in the chatroom!")
+        # client_socket.send(("Joined the server").encode('ascii'))
         
         # Create a thread for this specific client
-        client_thread = threading.Thread(target=handle, args=(client_socket,))
+        client_thread = threading.Thread(target=handle, args=(client_socket, client_address,))
         client_thread.start()
 
 # The main function will be used to start the server
@@ -64,24 +170,3 @@ def main():
     
 if __name__=='__main__':
     main()
-
-# The JOIN function will add a client to the chatroom
-def JOIN(username):
-    return
-
-# The LIST function will display all active users in the chatroom
-def LIST():
-    return
-
-# The MESG function will send a private message to the username provided
-def MESG(username, message):
-    return
-
-# The BCST function will broadcast a message to all active users
-def BCST(message):
-    return
-
-# The QUIT function will disconnect the user and remove them from the active 10 users
-def QUIT():
-    return
-
